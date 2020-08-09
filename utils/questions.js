2e27c9e2b1e7061d9596952b09369d7c9ef463b0
Dan Swan <rodger.swan@gmail.com>
2020-08-09 21:29:41 +1000

    starting questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,32 +1,22 @@
-const validation = require("./validation.js");
-
 const startQuestions = [
   {
     type: "list",
-    name: "init",
+    name: "menu",
     message: "What would you like to do?",
     choices: [
-      { name: "view all employees", value: "viewAll" },
-      { name: "view employees by department", value: "viewEmployeeDep" },
-      { name: "view employees by role", value: "viewEmployeeRole" },
-      { name: "update existing employee", value: "updateEmployeeDetails" },
-      { name: "add a new employee", value: "addNewEmployee" },
-      { name: "add a new department", value: "addNewDepartment" },
+      { name: "view all employees", value: "viewAllEmps" },
+      { name: "view all departments", value: "viewAllDeps" },
+      { name: "view all roles", value: "viewAllRoles" },
+      // { name: "update existing employee's role", value: "updateEmpRole" },
+      // { name: "update existing employee's manager", value: "updateEmpMgr" },
+      // { name: "add a new employee", value: "addNewEmployee" },
+      // { name: "add a new department", value: "addNewDepartment" },
       { name: "add a new role", value: "addNewRole" },
-      { name: "delete a department", value: "deleteDep" },
-      { name: "delete a role", value: "deleteRole" },
-      { name: "delete an employee", value: "deleteEmp" },
+      // { name: "delete a department", value: "deleteDep" },
+      // { name: "delete a role", value: "deleteRole" },
+      // { name: "delete an employee", value: "deleteEmp" },
     ],
   },
 ];
-
-// const addEmployee = [
-//     {
-//         type: "input",
-//         name: "firstName",
-//         message: "What is the employee's first name?"
-
-// }
-// ]
 
 module.exports = { startQuestions };
