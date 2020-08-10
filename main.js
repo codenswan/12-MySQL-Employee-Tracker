4fbc50405a30d1264c14cf460eca513de33edf6d
Dan Swan <rodger.swan@gmail.com>
2020-08-10 22:02:37 +1000

    change sequence for main menu functions

--- main.js
+++ main.js
@@ -1,50 +1,54 @@
 const db = require("./models");
 const questions = require("./utils/questions.js");
-const validation = require("./utils/validation.js");
 const { prompt } = require("inquirer");
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 
 clear();
-console.log(
-  chalk.red(figlet.textSync("Staff Database", { horizontalLayout: "full" }))
-);
+// console.log(
+//   chalk.red(figlet.textSync("Staff Database", { horizontalLayout: "full" }))
+// );
 
 function init() {
   prompt(questions.startQuestions).then(async (answer) => {
+    clear();
     switch (answer.menu) {
       case "viewAllEmps":
         await db.findAllEmployees();
+       
         init();
-        clear();
         break;
 
       case "viewAllDeps":
         await db.findAllDepartments();
         init();
-        clear();
         break;
 
       case "viewAllRoles":
         await db.findAllRoles();
         init();
-        clear();
         break;
-      
+
       case "addNewEmployee":
         await db.addEmployee();
+        init();
+        break;
 
       case "addNewDepartment":
         await db.addDepartment();
         init();
         clear();
         break;
-      
+
       case "addNewRole":
         await db.addRole();
         init();
-        clear();
+        break;
+      
+      case "updateEmpRole":
+        await db.updateEmployee();
+        init();
         break;
     }
   });
