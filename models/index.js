a62e21ca417e4287b422f5b78b1e544cf6a61728
Dan Swan <rodger.swan@gmail.com>
2020-08-10 22:03:03 +1000

    add async/await to find functions and add employee

--- models/index.js
+++ models/index.js
@@ -10,67 +10,89 @@ class DB {
     });
   }
 
-  // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
-  findAllEmployees() {
-    const query =
+  //* Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+  async findAllEmployees() {
+    const viewEmps =
       "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id;";
-    return this.connection.query(query, (err, res) => {
-      if (err) throw err;
-      console.table(res);
-    });
+    const res = await this.connection.query(viewEmps);
+    console.table(res[0]);
   }
 
-  // find all departments
-  findAllDepartments() {
-    const query = "SELECT * FROM departments";
-    return this.connection.query(query, (err, res) => {
-      if (err) throw err;
-      console.table(res);
-    });
+  //* find all departments
+  async findAllDepartments() {
+    const viewDeps = "SELECT * FROM departments";
+    const res = await this.connection.query(viewDeps);
+    console.table(res[0]);
   }
 
-  //find all roles and display with department id and name
-  findAllRoles() {
-    const query =
+  //* find all roles and display with department id and name
+  async findAllRoles() {
+    const viewRoles =
       "SELECT * FROM roles LEFT JOIN departments on roles.department_id = departments.id";
 
-    this.connection.query(query, (err, res) => {
-      if (err) throw err;
-      console.table(res);
+    const res = await this.connection.query(viewRoles);
+    console.table(res[0]);
+  }
+
+  //* method to add a new employee with corresponding mgrs and roles.
+  async addEmployee() {
+    let mgrQuery = `SELECT employees.id, concat(employees.first_name, ' ' ,  employees.last_name) AS Manager FROM employees ORDER BY Manager ASC;`;
+    let roleQuery = `SELECT id, title FROM roles ORDER BY title ASC;`;
+    let roles = await this.connection.query(roleQuery);
+    let mgrs = await this.connection.query(mgrQuery);
+
+    let rolesList = roles[0].map((row) => {
+      return { name: row.title, value: row.id };
+    });
+
+    let mgrsList = mgrs[0].map((row) => {
+      return { name: row.Manager, value: row.id };
+    });
+
+    return prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the first name of the employee?",
+        validation: validation.stringCapital,
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the last name of the employee?",
+        validation: validation.stringCapital,
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What is the role of this employee?",
+        choices: rolesList,
+      },
+      {
+        type: "list",
+        name: "mgr",
+        message: "Who is the manager of this employee?",
+        choices: mgrsList,
+      },
+    ]).then((answer) => {
+      console.log(answer);
+      let query = "INSERT INTO employees set ?;";
+      let values = {
+        first_name: answer.firstName,
+        last_name: answer.lastName,
+        role_id: answer.role,
+        manager_id: answer.mgr,
+      };
+      this.connection.query(query, values, (err) => {
+        if (err) throw err;
+      });
+      console.log(
+        `Adding ${answer.firstName} ${answer.lastName} to employees table.`
+      );
     });
   }
 
-  addEmployee() {
-    let [rows, fields] = this.connection
-      .execute(`Select id, name FROM departments ORDER BY name ASC;`)
-    
-    let depList = rows.map((row) => {
-          return row.name;
-        });
-    
-    console.log(depList);
-
-    // return prompt([
-    //   {
-    //     type: "input",
-    //     name: "firstName",
-    //     message: "What is the first name of the employee?",
-    //     validation: validation.stringCapital,
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "lastName",
-    //     message: "What is the last name of the employee?",
-    //     validation: validation.stringCapital,
-    //   },
-    //   {
-    //     type: "list",
-    //     name: "department",
-    //     choices: depList,
-    //   },
-    // ]);
-  }
-
+  //* method adds a new department.
   addDepartment() {
     return prompt([
       {
@@ -130,6 +152,24 @@ class DB {
           if (err) throw err;
         });
         console.log(`Adding ${answer.role} to roles table.`);
+      });
+    });
+  }
+
+  updateEmployee() {
+    let empQuery = `SELECT employees.id, concat(employees.first_name, ' ' ,  employees.last_name) AS Employee FROM employees ORDER BY Employee ASC;`;
+    let roleQuery = `SELECT id, title FROM roles ORDER BY title ASC;`;
+    return this.connection.query(roleQuery, (err, roles) => {
+      this.connection.query(empQuery, (err, emps) => {
+        if (err) throw err;
+        let rolesList = roles.map((row) => {
+          return row.title;
+        });
+        let empsList = emps.map((row) => {
+          return row.Employee;
+        });
+        console.table(rolesList);
+        console.table(empsList);
       });
     });
   }
