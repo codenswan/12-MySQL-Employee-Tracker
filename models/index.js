c879fe3bb7803e77eceaf8fe88178354611a969e
Dan Swan <rodger.swan@gmail.com>
2020-08-11 14:02:03 +1000

    add employee function works

--- models/index.js
+++ models/index.js
@@ -75,14 +75,13 @@ class DB {
         choices: mgrsList,
       },
     ]).then((answer) => {
-      console.log(answer);
       let query = "INSERT INTO employees set ?;";
-      let values = {
+      let values = [{
         first_name: answer.firstName,
         last_name: answer.lastName,
         role_id: answer.role,
         manager_id: answer.mgr,
-      };
+      }];
       this.connection.query(query, values, (err) => {
         if (err) throw err;
       });
@@ -93,8 +92,8 @@ class DB {
   }
 
   //* method adds a new department.
-  addDepartment() {
-    return prompt([
+  async addDepartment() {
+    await prompt([
       {
         type: "input",
         name: "newDep",
@@ -109,70 +108,100 @@ class DB {
     });
   }
 
-  addRole() {
+  async addRole() {
     const query = `Select id, name FROM departments ORDER BY name ASC;`;
-    return this.connection.query(query, (err, departments) => {
-      if (err) throw err;
-      let depList = departments.map((row) => {
-        return row.name;
-      });
-
-      prompt([
-        {
-          type: "input",
-          name: "role",
-          message: "Enter the title of the new role:",
-          validation: validation.string,
-        },
-        {
-          type: "input",
-          name: "salary",
-          message: "Enter the employee's salary",
-          validation: validation.number,
-        },
-        {
-          type: "list",
-          name: "department",
-          choices: depList,
-        },
-      ]).then((answer) => {
-        for (let index = 0; index < departments.length; index++) {
-          if (departments[index].name === answer.department) {
-            answer.department_id = departments[index].id;
-          }
-        }
-
-        let query2 = "INSERT INTO roles set ?;";
-        const values = {
-          title: answer.role,
-          salary: answer.salary,
-          department_id: answer.department_id,
-        };
-        this.connection.query(query2, values, (err, res) => {
-          if (err) throw err;
-        });
-        console.log(`Adding ${answer.role} to roles table.`);
-      });
+    let deps = await this.connection.query(query);
+    let depList = deps[0].map((row) => {
+      return { name: row.name, value: row.id };
     });
-  }
 
-  updateEmployee() {
-    let empQuery = `SELECT employees.id, concat(employees.first_name, ' ' ,  employees.last_name) AS Employee FROM employees ORDER BY Employee ASC;`;
-    let roleQuery = `SELECT id, title FROM roles ORDER BY title ASC;`;
-    return this.connection.query(roleQuery, (err, roles) => {
-      this.connection.query(empQuery, (err, emps) => {
+    return prompt([
+      {
+        type: "input",
+        name: "role",
+        message: "Enter the title of the new role:",
+        validation: validation.string,
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Enter the employee's salary",
+        validation: validation.number,
+      },
+      {
+        type: "list",
+        name: "department",
+        choices: depList,
+      },
+    ]).then((answer) => {
+      let query2 = "INSERT INTO roles set ?;";
+      const values = {
+        title: answer.role,
+        salary: answer.salary,
+        department_id: answer.department,
+      };
+      this.connection.query(query2, values, err => {
         if (err) throw err;
-        let rolesList = roles.map((row) => {
-          return row.title;
-        });
-        let empsList = emps.map((row) => {
-          return row.Employee;
-        });
-        console.table(rolesList);
-        console.table(empsList);
       });
+      console.log(`Adding ${answer.role} to roles table.`);
     });
   }
+
+  async updateEmployeeRole() {
+    let empQuery = `SELECT employees.id, concat(employees.first_name, ' ' ,  employees.last_name) AS Employee FROM employees ORDER BY Employee ASC;`;
+    let emps = await this.connection.query(empQuery);
+    let emplist = emps[0].map((row) => {
+      return { name: row.Employee, value: row.id };
+    });
+
+    let roleQuery = `SELECT id, title FROM roles ORDER BY title ASC;`;
+    let roles = await this.connection.query(roleQuery);
+    let rolesList = roles[0].map((row) => {
+      return { name: row.title, value: row.id };
+    });
+
+    return prompt([
+      {
+        type: "list",
+        name: "emps",
+        message: "Please select the employee to update:",
+        choices: emplist,
+      },
+      {
+        type: "list",
+        message: "What is their new role?",
+        name: "roles",
+        choices: rolesList,
+      },
+    ]).then((answer) => {
+      let updateQuery = "UPDATE employees SET role_id = ? WHERE id = ?;";
+      let values = {
+        role_id: answer.roles,
+        id: answer.emps,
+      };
+      this.connection.query(updateQuery, values, (err) => {
+        if (err) throw err;
+      });
+      console.log(`Changing employee's role.`);
+    });
+  }
+
+  async updateEmployeeMgr() {
+    let mgrQuery = `SELECT employees.id, concat(employees.first_name, ' ' ,  employees.last_name) AS Manager FROM employees ORDER BY Manager ASC;`;
+    let mgrs = await this.connection.query(mgrQuery);
+    let mgrsList = mgrs[0].map((row) => {
+      return { name: row.Manager, value: row.id };
+    });
+
+    return prompt([
+      {
+        type: "list",
+        message: "Who is their new manager?",
+        choice: mgrsList,
+      },
+    ]).then( answer => {console.log(answer);})
+  }
+
 }
 
 module.exports = new DB(connection);
